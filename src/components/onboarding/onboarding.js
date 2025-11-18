0d66c5918687c7da31f5a74800a07b99bf0b3ee9
mdghdev81 <mdgh.dev81@gmail.com>
2025-11-18 23:10:17 +0330

    fix:fix bugs

--- src/components/onboarding/onboarding.js
+++ src/components/onboarding/onboarding.js
@@ -2,6 +2,31 @@ import { Onboarding1 } from "./onboarding1.js";
 import { Onboarding2 } from "./onboarding2.js";
 
 export function Onboarding() {
-	return Onboarding2();
+	return Onboarding1();
 }
-console.log(Onboarding2());
+
+
+
+
+// داداشم چت چی پی تی
+// import { El } from "../../utils/el.js";
+// import { Onboarding1 } from "./onboarding1.js";
+// import { Onboarding2 } from "./onboarding2.js";
+
+// export function Onboarding() {
+// 	const container = El({
+// 		element: "div",
+// 	});
+
+// 	// اول: نمایش Onboarding1
+// 	const first = Onboarding1();
+// 	container.appendChild(first);
+
+// 	// بعد از 5 ثانیه: نمایش Onboarding2
+// 	setTimeout(() => {
+// 		container.innerHTML = ""; // پاک کردن صفحه اول
+// 		container.appendChild(Onboarding2()); // نمایش صفحه دوم
+// 	}, 5000);
+
+// 	return container;
+// }

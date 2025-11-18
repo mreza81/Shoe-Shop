8b8c27f85901c36db85770a9e51c75c49217e9c2
mdghdev81 <mdgh.dev81@gmail.com>
2025-11-18 16:37:37 +0330

    feat:create onboarding1

--- src/components/onboarding/onboarding1.js
+++ src/components/onboarding/onboarding1.js
@@ -2,17 +2,35 @@ import { El } from "../../utils/el.js";
 export function Onboarding1() {
 	return El({
 		element: "div",
-		className: "bg-white w-full h-full text-black",
+		className: "bg-white w-full h-full",
 		children: [
 			El({
 				element: "div",
-				className: "icon",
+				className:
+					"icon flex justify-center items-center gap-3 mt-[392px] w-[231px] mx-[98px]",
 				children: [
 					El({
-						element: "img",
-						src: "",
+						element: "div",
+						className:
+							"bg-black rounded-[50%] w-[59px] h-[59px] flex justify-center items-center ",
+						children: [
+							El({
+								element: "img",
+								src: "public/assets/images/Vector 1.png",
+							}),
+						],
+					}),
+					El({
+						element: "p",
+						innerText: "Shoea",
+						className: "text-[52px] font-bold ",
 					}),
 				],
+			}),
+			El({
+				element: "img",
+				src: "public/assets/images/Spinner-1s-200px.svg",
+				className: "w-16 h-16 mt-[306px] mx-[190px]",
 			}),
 		],
 	});

9bacf2f87462e3776043ba34e413fb011e64001a
mdghdev81 <mdgh.dev81@gmail.com>
2025-11-21 22:58:48 +0330

    fix:fix bugs in signup api

--- src/API/signup.js
+++ src/API/signup.js
@@ -1,20 +1,20 @@
+import { BASE_URL } from "./BASE_URL";
+
 export async function signup() {
 	const userSignup = document.querySelector("#userName-Signup");
 	const passSignup = document.querySelector("#password-signup");
 	const userName = userSignup.value;
 	const password = passSignup.value;
 	try {
-		const response = await fetch("http://localhost:3000/auth/signup", {
+		const response = await fetch(`${BASE_URL}/auth/signup`, {
 			method: "POST",
 			body: JSON.stringify({ username: userName, password: password }),
 			headers: { "Content-Type": "application/json; charset=UTF-8" },
 		});
 		const data = await response.json();
 		console.log(data);
-		// console.log(data.user.username);
 
 		if (response.ok) {
-			localStorage.setItem("token", data.token);
 			alert("sign up sucess full ! please login");
 		} else {
 			alert(data.message);
@@ -22,4 +22,6 @@ export async function signup() {
 	} catch {
 		console.log("eror");
 	}
+	userSignup.value = "";
+	passSignup.value = "";
 }

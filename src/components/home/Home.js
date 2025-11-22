b973ee0ddda3f38fb295ffbe8142917f1bf54f82
mdghdev81 <mdgh.dev81@gmail.com>
2025-11-22 23:26:40 +0330

    fix:update homeEl component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,12 +3,132 @@ import { El } from "../../utils/el";
 export function Home() {
 	return El({
 		element: "div",
-		className: "home-container",
+		className: "home-container px-6",
 		children: [
 			El({
 				element: "div",
-				className: "home-header",
-				children: "",
+				className: "home-header h-20 flex justify-between items-center",
+				children: [
+					El({
+						element: "div",
+						className: "welcome w-[130px] h-12",
+						children: [
+							El({
+								element: "p",
+								innerText: "Good Morning 👋",
+								className: "text-4 font-medium text-[#757475]",
+							}),
+							El({
+								element: "p",
+								innerText: "Saeed Dehghani",
+								className: "text-4 font-bold text-[#152536] mt-2.5",
+							}),
+						],
+					}),
+					El({
+						element: "div",
+						className: " flex justify-center items-center gap-4 mt-[7px]",
+						children: [
+							El({
+								element: "img",
+								className: "w-6 h-6",
+								src: "public/assets/images/bell.svg",
+							}),
+							El({
+								element: "img",
+								className: "w-6 h-6",
+								src: "public/assets/images/heart.svg",
+							}),
+						],
+					}),
+				],
+			}),
+			El({
+				element: "input",
+				type: "text",
+				className:
+					"w-full h-[37px] bg-[#FAFAFA] mt-6 px-[34px] rounded-1 text-[14px] cursor-pointer",
+				placeholder: "Search",
+			}),
+			El({
+				element: "img",
+				className: "absolute left-9 top-[114px]",
+				src: "public/assets/images/search.svg",
+			}),
+			El({
+				element: "div",
+				className: "mt-[30px] flex justify-between items-center",
+				children: [
+					El({
+						element: "p",
+						className: "text-[20px] font-semibold text-[#152536]",
+						innerText: "Most Popular",
+					}),
+					El({
+						element: "p",
+						className: "text-4 font-semibold text-[#152536]",
+						innerText: "See All",
+					}),
+				],
+			}),
+			El({
+				element: "div",
+				className:
+					"brands flex justify-start items-center gap-3 mt-5 -mr-6 overflow-scroll no-scrollbar ",
+				children: [
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer  ",
+						innerText: "All",
+					}),
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer ",
+						innerText: "Nike",
+					}),
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer ",
+						innerText: "Adidas",
+					}),
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer",
+						innerText: "Puma",
+					}),
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer ",
+						innerText: "Asics",
+					}),
+					El({
+						element: "button",
+						type: "button",
+						className:
+							"cursor-pointer h-[39px] px-5 text-4 font-semibold border-2 border-[#343A40] rounded-[25px] cursor:pointer ",
+						innerText: "NewBalance",
+					}),
+				],
+			}),
+			El({
+				element: "div",
+				id: "items-div",
+				className: "mt-6",
+				children: [
+					El({
+						element: "div",
+					}),
+				],
 			}),
 		],
 	});
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import { OnboardingPage } from "./pages/onboardingPage.js";
 Swiper.use([Navigation, Pagination]);
-
 import "./style.css";
 import { router } from "./utils/router.js";
 import { AuthenticationpPage } from "./pages/AuthenticationPage.js";
+import { HomePage } from "./pages/homePage.js";
 
 const app = document.getElementById("app");
 const container = document.createElement("div");
@@ -14,5 +14,46 @@ container.append(OnboardingPage());
 
 router.addRoute("/onboarding", OnboardingPage);
 router.addRoute("/signup", AuthenticationpPage);
+router.addRoute("/", HomePage);
 router.init(container);
-localStorage.removeItem("user");
+
+const brands = document.querySelector(".brands");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+brands.addEventListener("mousedown", (e) => {
+	isDown = true;
+	brands.classList.add("active");
+	startX = e.pageX - brands.offsetLeft;
+	scrollLeft = brands.scrollLeft;
+});
+
+brands.addEventListener("mouseleave", () => {
+	isDown = false;
+});
+
+brands.addEventListener("mouseup", () => {
+	isDown = false;
+});
+
+brands.addEventListener("mousemove", (e) => {
+	if (!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - brands.offsetLeft;
+	const walk = (x - startX) * 1.5;
+	brands.scrollLeft = scrollLeft - walk;
+});
+
+// تاچ‌پد / موبایل
+brands.addEventListener("touchstart", (e) => {
+	startX = e.touches[0].clientX;
+	scrollLeft = brands.scrollLeft;
+});
+
+brands.addEventListener("touchmove", (e) => {
+	const x = e.touches[0].clientX;
+	const walk = (x - startX) * 2;
+	brands.scrollLeft = scrollLeft - walk;
+});

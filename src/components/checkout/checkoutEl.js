a5778f82ca4b5d77201a4d27c02c702ddc00ee21
mdghdev81 <mdgh.dev81@gmail.com>
2025-11-27 14:07:29 +0330

    fix:fix bugs

--- src/components/checkout/checkoutEl.js
+++ src/components/checkout/checkoutEl.js
@@ -1,5 +1,29 @@
 import { El } from "../../utils/el";
 
 export function checkoutEl() {
-	return El({});
+	return El({
+		element: "div",
+		className: "checkout-container px-6 pt-",
+		children: [
+			El({
+				element: "div",
+				className: "checkout-header w-full flex justify-between items-center",
+				children: [
+					El({
+						element: "div",
+						className: "co-header-left",
+						children: [
+							El({
+								element: "img",
+							}),
+						],
+					}),
+					El({
+						element: "div",
+						className: "co-header-right",
+					}),
+				],
+			}),
+		],
+	});
 }
